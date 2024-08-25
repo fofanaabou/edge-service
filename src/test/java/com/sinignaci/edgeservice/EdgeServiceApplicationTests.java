f620ac4ff724bc45e08ac5ba93e86854e717af13
aboufofana <aboufofana76@gmail.com>
2024-08-25 22:10:14 +0200

    configure ci/cd

--- src/test/java/com/sinignaci/edgeservice/EdgeServiceApplicationTests.java
+++ src/test/java/com/sinignaci/edgeservice/EdgeServiceApplicationTests.java
@@ -1,23 +1,32 @@
 package com.sinignaci.edgeservice;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
-import java.util.Objects;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-@SpringBootTest
+@Testcontainers
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class EdgeServiceApplicationTests {
 
-	@Autowired
-	ApplicationContext context;
+	private static final int REDIS_PORT = 6379;
 
 	@Test
+	@SuppressWarnings("uncheked")
 	void contextLoads() {
+	}
 
-		assertTrue(Objects.nonNull(context.getBean("edgeServiceApplication", EdgeServiceApplication.class)));
+	@Container
+	static GenericContainer<?> redis = new GenericContainer<>(DockerImageName.parse("redis:latest"))
+			.withExposedPorts(REDIS_PORT);
+
+	@DynamicPropertySource
+	static void redisProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.redis.host", () -> redis.getHost());
+		registry.add("spring.redis.port", () -> redis.getMappedPort(REDIS_PORT));
 	}
 }
