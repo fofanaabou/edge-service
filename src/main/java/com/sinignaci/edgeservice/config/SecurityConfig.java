33346759becf58c9cf5347261c41ecb0d25263ec
aboufofana <aboufofana76@gmail.com>
2024-08-31 20:05:22 +0200

    add security

--- src/main/java/com/sinignaci/edgeservice/config/SecurityConfig.java
+++ src/main/java/com/sinignaci/edgeservice/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.sinignaci.edgeservice.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
@@ -10,6 +9,8 @@ import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.client.web.server.WebSessionServerOAuth2AuthorizedClientRepository;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint;
 import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
@@ -17,7 +18,6 @@ import org.springframework.security.web.server.csrf.CsrfToken;
 import org.springframework.web.server.WebFilter;
 import reactor.core.publisher.Mono;
 
-@Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
@@ -38,7 +38,6 @@ public class SecurityConfig {
                 .build();
     }
 
-
     WebFilter csrfWebFilter() {
         return (exchange, chain) -> {
             exchange.getResponse().beforeCommit(() -> Mono.defer(() -> {
@@ -54,5 +53,10 @@ public class SecurityConfig {
         var oidcLogoutSuccessHandler = new OidcClientInitiatedServerLogoutSuccessHandler(clientRegistrationRepository);
         oidcLogoutSuccessHandler.setPostLogoutRedirectUri("{baseUrl}");
         return oidcLogoutSuccessHandler;
+    }
+
+    @Bean
+    ServerOAuth2AuthorizedClientRepository auth2AuthorizedClientRepository() {
+        return new WebSessionServerOAuth2AuthorizedClientRepository();
     }
 }
