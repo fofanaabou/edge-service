a8080b871a5869a89869d1ddbfaa1f080de7447e
aboufofana <aboufofana76@gmail.com>
2024-09-09 00:16:02 +0200

    update security

--- src/main/java/com/sinignaci/edgeservice/config/SecurityConfig.java
+++ src/main/java/com/sinignaci/edgeservice/config/SecurityConfig.java
@@ -38,6 +38,7 @@ public class SecurityConfig {
                 .build();
     }
 
+    @Bean
     WebFilter csrfWebFilter() {
         return (exchange, chain) -> {
             exchange.getResponse().beforeCommit(() -> Mono.defer(() -> {
